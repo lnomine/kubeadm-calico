619b2b57aa808f18ee8a6a72fd459825ea1f0689
Laurent Nominé <laurent.nomine@gmail.com>
2022-12-05 14:16:55 +0100

    Update and rename setup-node.sh to prereqs.sh

--- prereqs.sh
+++ prereqs.sh
@@ -23,13 +23,3 @@ EOF
 sysctl --system
 
 ln -s /opt/cni/bin/ /usr/lib/cni
-
-if [ ! -z "$2" ];
-then
-kubeadm init --kubernetes-version $1 --control-plane-endpoint "${loadbalancer}:5443" --upload-certs --pod-network-cidr=192.168.0.0/16
-export KUBECONFIG=/etc/kubernetes/admin.conf
-echo "Waiting for Kubernetes API..."
-sleep 30
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/$2/manifests/tigera-operator.yaml
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/$2/manifests/custom-resources.yaml
-fi
